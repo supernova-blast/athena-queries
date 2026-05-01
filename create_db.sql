38ff0d6de778822e684a63a0e61c84ed744abb60
Mitja <whatupdoc@yandex.com>
2026-05-01 14:47:16 +0400

    Refine comments in create_db.sql for clarity
    Updated comments and instructions for creating an Athena database and table for CloudTrail logs.

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,21 @@
--- first, create an S3 bucket in which the query results will be stored; e.g.: s3://athena-query-results-1234567890/
+/*
+DESCRIPTION: Athena CloudTrail Database and Table Setup
 
--- then run in Athena (run these 2 CREATE commands separately, Athena does no allow to run serveral queries in one go):
+This script creates an Athena database and external table for querying AWS CloudTrail logs stored in S3.
+Before running it, configure an Athena query result location, preferably in a separate S3 prefix from the CloudTrail log files.
+
+Before running:
+- Create or choose an S3 location for Athena query results, for example:
+  s3://athena-query-results-1234567890/
+- Replace cloudtrail_log_full with your preferred Athena database name.
+- Replace events with your preferred table name if needed.
+- Replace s3://<s3-bucket-name-for-cloudtrail-log-full>/AWSLogs/<account-id>/ with your CloudTrail S3 log location.
+- Run CREATE DATABASE and CREATE EXTERNAL TABLE separately, because Athena usually runs one statement at a time.
+*/
+
 CREATE DATABASE cloudtrail_log_full;  -- you can give it any name
 
--- then create table named 'events' (or choose another name for the table)
-CREATE EXTERNAL TABLE cloudtrail_log_full.events (
+CREATE EXTERNAL TABLE cloudtrail_log_full.events (  -- you can choose another name for the table
     eventversion STRING,
     useridentity STRUCT<
         type: STRING,
@@ -54,10 +65,3 @@ STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
 LOCATION 's3://<s3-bucket-name-for-cloudtrail-log-full>/AWSLogs/<account-id>/'  -- enter the S3 location to which your CloudTrail writes
 TBLPROPERTIES ('classification'='cloudtrail');
-
-/*
-Note: you can use the same S3 bucket where CloudTrail writes logs as the query result location for Athena, 
-but it's not recommended as a best practice. However, if using the same bucket, create separate prefixes:
-CloudTrail logs: s3://<s3-bucket-name-for-cloudtrail-log-full>/AWSLogs/
-Athena results: s3://<s3-bucket-name-for-cloudtrail-log-full>/AthenaResults/
-*/
